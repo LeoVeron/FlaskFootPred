6f22a1cb8b3f165806ea87b9c5d52bbe65bdfa56
LeoVeron <leovip.veron@gmail.com>
2021-06-07 12:51:32 +0200

    heroku4

--- app/__init__.py
+++ app/__init__.py
@@ -38,5 +38,5 @@ def create_app():
 
 def create_database(app):
     if not path.exists('app/' + DB_NAME):
-        db.create_all(app=app)
+        db.create_all(app=app, checkfirst=True)
         print('Created Database!')
