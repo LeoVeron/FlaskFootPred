0ef32722c798d08a76e5f59b58ee987d1a96df0c
LeoVeron <leovip.veron@gmail.com>
2021-06-07 10:10:16 +0200

    heroku fix

--- app/__init__.py
+++ app/__init__.py
@@ -21,15 +21,14 @@ def create_app():
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    create_database(app)
     
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     from .models import User
-
+    create_database(app)
+    
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
