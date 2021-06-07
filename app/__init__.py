4bbae6fa9422b35aa0b3937e4e46b55d94aafed7
LeoVeron <leovip.veron@gmail.com>
2021-06-07 12:47:15 +0200

    heroku3

--- app/__init__.py
+++ app/__init__.py
@@ -22,12 +22,12 @@ def create_app():
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
+    from .models import User
+    create_database(app)
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
-    from .models import User
-    create_database(app)
     
     @login_manager.user_loader
     def load_user(user_id):
